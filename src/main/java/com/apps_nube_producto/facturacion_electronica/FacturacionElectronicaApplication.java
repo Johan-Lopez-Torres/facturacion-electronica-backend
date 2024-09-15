eb29cba1091d2c002de97fc4cfb23d60b6b47922
Johan Lopez <johanpppp222345@gmail.com>
2024-09-14 20:54:37 -0500

    codigo base

---
+++ src/main/java/com/apps_nube_producto/facturacion_electronica/FacturacionElectronicaApplication.java
@@ -0,0 +1,13 @@
+package com.apps_nube_producto.facturacion_electronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionElectronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionElectronicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps_nube_producto/facturacion_electronica/FacturacionElectronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps_nube_producto.facturacion_electronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionElectronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
